262731b9c16e6b997ec7a1a92dcad26dc823de64
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-11-17 16:34:54 +0600

    modify value error practice.py

--- CS50P/Visual Studio/Practices/value error practice.py
+++ CS50P/Visual Studio/Practices/value error practice.py
@@ -1,28 +1,32 @@
-# i = 1
-# while i < 3:
-#     try:
-#         x = int(input("What's x?\n> "))
-#         print(f"Your given number is {x}")
-#     except ValueError:
-#         print("x is not an integer.")
-#         if i == 1:
-#             print(f"{i}st attempt failed.")
-#         else:
-#             print(f"{i}nd attempt failed.")
-#     else:
-#         break
+# infinity question asking until get the right below
 
-i = 1
-
-while i <= 3:
+while True:
     try:
-        x = int((input("What's x?\n> ")))
-        print(f"You have inputed {x}")
-            
+        x = int(input("What's x?\n> "))
+        print(f"Your given number is {x}")
+    except ValueError:
+        print("x is not an integer.")
+    else:
+        break
+
+    """
+    this loop will be happening 5 times until the user input is not an integer value.
+    also, it shows how many attempts the user takes
+    """
+i = 1
+while i <= 5:
+    try:
+        x = int((input("Enter an integer value: ")))
+        print(f"You have inputted {x}")
     except ValueError:
         print("x is not integer.")
         if i == 1:
              print(f"{i}st attempt failed.")
-        else:
+        elif i <= 3:
             print(f"{i}nd attempt failed.")
-        i += 1
+        else:
+            print(f"{i}th attempt failed.")
+    i += 1
+        
+        
+        

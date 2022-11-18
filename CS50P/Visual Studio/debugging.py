25a46a645984ccc4f495e399d4f1cfa7dcee4fdb
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-11-18 22:55:38 +0600

    modified

--- CS50P/Visual Studio/debugging.py
+++ CS50P/Visual Studio/debugging.py
@@ -7,8 +7,10 @@ def main():
     
 def pyramid(n):
     for i in range(n):
-        print(i + 1 , end="  ")
+        print(i + 1 , end="   ")
         print("#" * (i + 1))
         
 # if __name__ == "__main__":
+#     main()
+    
 main()

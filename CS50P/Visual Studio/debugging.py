a01365fcf01ba72fe6a58551596477f6a9f4f9de
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-11-18 22:53:26 +0600

    debugging

---
+++ CS50P/Visual Studio/debugging.py
@@ -0,0 +1,14 @@
+"""Code of super mario"""
+
+def main():
+    height = int(input("Enter the height : "))
+    pyramid(height)
+    
+    
+def pyramid(n):
+    for i in range(n):
+        print(i + 1 , end="  ")
+        print("#" * (i + 1))
+        
+# if __name__ == "__main__":
+main()

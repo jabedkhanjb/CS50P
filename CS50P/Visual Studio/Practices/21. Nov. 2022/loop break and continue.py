bd06fad0d08a54d8f5d367339deb54b7fad9d166
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-11-21 15:05:17 +0600

    break and continue

--- CS50P/Visual Studio/Practices/21. Nov. 2022/loop break and continue.py
+++ CS50P/Visual Studio/Practices/21. Nov. 2022/loop break and continue.py
@@ -1,8 +1,22 @@
-name = input("Enter your first name : ").strip()
+name = input("Enter your name : ").strip()
 target = input("Put your targetted letter : ").strip()
 print(name)
 for i in name:
     if i == target:
         break
+    # after this line, execution would be terminated
     print(i, end="")
 print(f"\nLoop terminated when '{i}' was found in the name.")        
+
+"""When we will use continue, execution will be continuing except that condition in entire process"""
+
+name = input("Enter name : ").strip()
+target = input("Put your targetted clause of the name : ")
+    
+for i in name:
+    if i == target:
+        continue
+    print(i, end="")
+print(f"\nName has been printed except '{target}'")
+
+    

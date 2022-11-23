8f05e2be5944c2d5400600cb3bcc2b284ef293ca
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-11-23 22:37:17 +0600

    cowsay library & API

---
+++ CS50P/Visual Studio/Lecture 4 Libraries/itunes.py
@@ -0,0 +1,13 @@
+import json
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit()
+    
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+print(json.dumps(response.json(), indent = 2))
+
+o =  response.json()
+for result in o["results"]:
+    print(result["trackName"])

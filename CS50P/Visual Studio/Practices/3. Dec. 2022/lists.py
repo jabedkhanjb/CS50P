596becefdb32230385d5542aa6d6b09a83f7b560
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-12-03 19:35:43 +0600

    L6 list practice 3. Dec. 2022

--- CS50P/Visual Studio/Practices/3. Dec. 2022/lists.py
+++ CS50P/Visual Studio/Practices/3. Dec. 2022/lists.py
@@ -4,5 +4,8 @@ for _ in range(2):
     # name = input("What's your name? ")
     names.append(input("What's your name? "))
     
-for myname in names:
-    print(myname)
+# for myname in names:
+#     print(myname)
+
+for name in sorted(names):
+    print(name)

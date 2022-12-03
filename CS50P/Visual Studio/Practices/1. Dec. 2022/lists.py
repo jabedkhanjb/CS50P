013da131113c6e743ea0482d40faba90e874f3f5
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-12-03 19:33:07 +0600

    L6 list practice 3. Dec. 2022

---
+++ CS50P/Visual Studio/Practices/1. Dec. 2022/lists.py
@@ -0,0 +1,6 @@
+name = []
+for i in range(5):
+    name.append(input("What's your name? ").title())
+    
+for name in name:
+    print(f"Hello, {name}")
---
+++ CS50P/Visual Studio/Practices/3. Dec. 2022/lists.py
@@ -0,0 +1,8 @@
+names = []
+
+for _ in range(2):
+    # name = input("What's your name? ")
+    names.append(input("What's your name? "))
+    
+for myname in names:
+    print(myname)

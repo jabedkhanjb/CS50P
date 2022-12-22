ca8f00ab1d38095592829577ffeb90fdce86b911
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-12-22 21:52:41 +0600

    L7 Regular Expressions Re Library

--- CS50P/Visual Studio/Lecture 7 Regular Expressions/validation.py
+++ CS50P/Visual Studio/Lecture 7 Regular Expressions/validation.py
@@ -1,1 +1,17 @@
-email = input("What's your email?").strip()
+email = input("What's your email?\n").strip()
+"""
+if "@" in email and "." in email:
+    print("Valid Email")
+    
+else:
+    print("Invalid Email Address")"""
+    
+username, domain = email.split("@")
+
+"""if username and "." in domain:
+    print("Valid")"""
+if username and domain.endswith(".edu"):
+    print("Valid")
+    
+else:
+    print("Invalid")

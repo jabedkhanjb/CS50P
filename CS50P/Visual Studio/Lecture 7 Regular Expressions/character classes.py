83374711221cac7f3fddf816e2689b641295c73c
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-12-31 01:29:48 +0600

    chracter classess note.txt file modified

--- CS50P/Visual Studio/Lecture 7 Regular Expressions/character classes.py
+++ CS50P/Visual Studio/Lecture 7 Regular Expressions/character classes.py
@@ -6,3 +6,14 @@ if re.search(r"^\w+@\w+\.edu$", email):
     print("Valid Email")
 else:
     print("Invalid Email")
+    
+    """
+    reference to character classes
+\d  decimal digit 
+\D  not a decimal digit
+\s  whitespace characters 
+\S  not a whitespace character
+\w  word character ... as well as numebers and the underscore 
+\W  not a word character 
+    
+    """

7991dd38757853d1594ebce30ca0be91aa081652
jabedkhanjb <mahfuzislam1410@gmail.com>
2022-12-31 01:25:56 +0600

    new file

--- CS50P/Visual Studio/Lecture 7 Regular Expressions/character classes.py
+++ CS50P/Visual Studio/Lecture 7 Regular Expressions/character classes.py
@@ -2,7 +2,7 @@ import re
 
 email = input("Hey, input your email address: \n").strip()
 
-if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$", email):
+if re.search(r"^\w+@\w+\.edu$", email):
     print("Valid Email")
 else:
     print("Invalid Email")

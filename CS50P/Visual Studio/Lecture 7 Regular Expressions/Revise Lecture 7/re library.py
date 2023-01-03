9082403322e4604de5a7f2f905e936e505056d67
jabedkhanjb <mahfuzislam1410@gmail.com>
2023-01-04 01:18:32 +0600

    re library in practice folder of lecture 7

---
+++ CS50P/Visual Studio/Lecture 7 Regular Expressions/Revise Lecture 7/re library.py
@@ -0,0 +1,8 @@
+import re
+
+email = input("Input your email addree: ").strip()
+
+if re.search("@", email):
+    print("Email is valid")
+else:
+    print("Email is invalid")
--- CS50P/Visual Studio/Lecture 7 Regular Expressions/Revise Lecture 7/validate.py
+++ CS50P/Visual Studio/Lecture 7 Regular Expressions/Revise Lecture 7/validate.py
@@ -1,14 +1,14 @@
 email = input("What's your email?\n").strip()
 
 if "@" in email and "." in email:
-    print(email,"is Valid")
+    print("\nYour email address",'"'+email+'"',"is Valid")
 else:
-    print(f"{email} is Invalid")
+    print("\nYour email address",'"'+f"{email}"+'"', "is Invalid")
     
 EMAIL = input("What's your email address?\n").strip()
 
 username, domain = EMAIL.split("@")
-
+print("Here is your username", username)
 if username and "." in domain:
     print("Valid Email")
 else:

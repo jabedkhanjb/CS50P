22360bcebde1071174930954e84bc81d0991e0f5
jabedkhanjb <mahfuzislam1410@gmail.com>
2023-01-04 01:34:32 +0600

    regular expression pattern

--- CS50P/Visual Studio/Lecture 7 Regular Expressions/Revise Lecture 7/regular expression pattern.py
+++ CS50P/Visual Studio/Lecture 7 Regular Expressions/Revise Lecture 7/regular expression pattern.py
@@ -2,7 +2,7 @@ import re
 
 email = input("Input your email address: ").strip()
 
-if re.search(".*@.*", email):
+if re.search(".*@.+", email):
     print("Email is valid")
 else:
     print("Email is invalid")

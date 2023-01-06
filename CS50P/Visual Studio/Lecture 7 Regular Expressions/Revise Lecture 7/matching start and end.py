e5c95699cb32ba364ab2a071c7094b1ec17106c4
jabedkhanjb <mahfuzislam1410@gmail.com>
2023-01-06 22:19:09 +0600

    updated

--- CS50P/Visual Studio/Lecture 7 Regular Expressions/Revise Lecture 7/matching start and end.py
+++ CS50P/Visual Studio/Lecture 7 Regular Expressions/Revise Lecture 7/matching start and end.py
@@ -2,7 +2,7 @@ import re
 
 email = input("Please, insert your email : ")
 
-if re.search(r"^[a-f0-5]+@[^@]+\.edu$", email):
+if re.search(r"^[a-zA-Z0-9_.]+@[a-zA-Z0-9_.]+\.edu$", email):
     # by using "^[^@]+@[^@]+\.edu$" this command it will
     # stop counting extra @ sign over here except one as a valid 
     # email, and the output will be invalid for sure for the redundancy of @ sign

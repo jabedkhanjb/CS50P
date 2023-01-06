47c0e67681dd6353e59987a57218f3dff346d492
jabedkhanjb <mahfuzislam1410@gmail.com>
2023-01-06 22:42:55 +0600

    file up to date

--- CS50P/Visual Studio/Lecture 7 Regular Expressions/Revise Lecture 7/character classes.py
+++ CS50P/Visual Studio/Lecture 7 Regular Expressions/Revise Lecture 7/character classes.py
@@ -1,10 +1,15 @@
 import re 
 
 email = input("Please, insert your email : ")
-if re.search(r"^\w+@[a-zA-Z0-9_.]+\.edu$", email):
+if re.search(r"^\w+@\w+\.edu$", email):
     # backslash w in this case represents a "word character"
     # which is commonly known as a alphanumeric symbol or the underscore
     print("This email is Valid")
 else:
     print("This email is Invalid")
     
+    
+#   ^   means "start matching at the beginning of the string"
+#   w   means "any word character"
+#   +   means "one or more"
+

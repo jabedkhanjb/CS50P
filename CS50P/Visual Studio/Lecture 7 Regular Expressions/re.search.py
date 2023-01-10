c163d1259cee3229600e6afa56f7870da5d3d940
jabedkhanjb <mahfuzislam1410@gmail.com>
2023-01-11 01:18:11 +0600

    re.search for rectifying re.sub

--- CS50P/Visual Studio/Lecture 7 Regular Expressions/re.search.py
+++ CS50P/Visual Studio/Lecture 7 Regular Expressions/re.search.py
@@ -1,8 +1,8 @@
 import re
 url = input("URL: ").strip()
 
-if matches := re.search(r"https?://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
-
+if matches := re.search(r"https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
+# r"https?://(?:www\.)?twitter\.(?:com|org)/(.+)$"
     print(f"Username: ", matches.group(1))
 else:
     print("Username: Invalid URL")

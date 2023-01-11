81bacb0951d38ba340c008c5035ec3d9d71fc190
jabedkhanjb <mahfuzislam1410@gmail.com>
2023-01-11 21:49:37 +0600

    hackerrank problem solving file added

--- CS50P/Visual Studio/Practices/11.Jan.2023/L7 Regular Expression/re.search.py
+++ CS50P/Visual Studio/Practices/11.Jan.2023/L7 Regular Expression/re.search.py
@@ -1,7 +1,7 @@
 import re
 url = input("URL: ").strip()
  
-if match := re.search(r"^(?:https?://)?(?:www\.)?twitter\.com/([a-z0-9_]+)", url):
+if match := re.search(r"^(?:https?://)?(?:www\.)?(?:twitter|hackerrank|linkedin|instagram|snapchat|soundcloud|youtube)\.(?:com|org|net)/(?:in/)?([a-z0-9_]+)", url):
     print(f"Username: ", match.group(1))
 else:
     print("Invalid URL")

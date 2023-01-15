9a73f9202413d726f59b4bcf2c11c1b05b1d51dd
jabedkhanjb <mahfuzislam1410@gmail.com>
2023-01-16 03:06:09 +0600

    modified l8

---
+++ CS50P/Visual Studio/Lecture 8 Object Oriented Programming/dictionaries.py
@@ -0,0 +1,17 @@
+def main():
+    student = get_student()
+    print(f"{student['name']} from {student['house']}")
+    
+    
+def get_student():
+    # student = {}
+    # student["name"] = input("Name: ")
+    # student["house"] = input("House: ")
+    # return student
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name" : name, "house" : house}
+
+
+if __name__ == "__main__":
+    main()

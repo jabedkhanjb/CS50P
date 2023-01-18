103df1e8474ef5d0c13ab272e681650c3d9922bc
Jabed Khan <mahfuzislam1410@gmail.com>
2023-01-19 01:38:50 +0600

    updated

---
+++ CS50P/Visual Studio/Lecture 8 Object Oriented Programming/properties, Getters, and Setters.py
@@ -0,0 +1,40 @@
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Dhaka", "Chittagong", "Lakshmipur"]:
+            raise ValueError("Invalid House")
+        self.name = name
+        self.house = house
+        
+        
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    
+    @property
+    def house(self):
+        return self.house
+    
+    @house.setter
+    def house(self, house):
+        if house not in ["Dhaka", "Chittagong", "Lakshmipur"]:
+            raise ValueError("Invalid House")
+        self.house = house
+        
+        
+def main():
+    student = get_student()
+    student.house = "Number Four, Privet Drive"
+    print(student)
+    
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+    
+     
+    
+    
+if __name__ == "__main__":
+    main()
+    

e98f8c7e221cb67031fb1ca3cd956083403a9fb0
Jabed Khan <mahfuzislam1410@gmail.com>
2023-01-18 12:14:45 +0600

    updated

--- CS50P/Visual Studio/Lecture 8 Object Oriented Programming/custom methods.py
+++ CS50P/Visual Studio/Lecture 8 Object Oriented Programming/custom methods.py
@@ -1,13 +1,9 @@
-# import sys
-
 class Student:
     def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Dhaka", "Chittagong", "Lakshmipur"]:
             raise ValueError("Invalid House")
-            # return None
-            # sys.exit("Missing name")
         self.name = name
         self.house = house
         self.patronus = patronus
@@ -16,10 +12,24 @@ class Student:
     def __str__(self):
         return f"{self.name} from {self.house}"
     
+    def Charm(self):
+        match self.patronus:
+            case "UK":
+                return "🙂"
+            case "Canada":
+                return "😮"
+            case "USA":
+                return "🥳"
+            case "Bangladesh":
+                return "🥺"
+            case _:
+                return "Don't Worry"
+            
+    
 def main():
     student = get_student()
-    # print(f"{student.name} from {student.house}")
-    print(student)
+    print("Expecto Patronum!")
+    print(student.Charm())
     
 def get_student():
     name = input("Name: ")
